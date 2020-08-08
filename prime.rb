5a432a12bf4ab03534aa837051a705bb53258e20
Marques Avery <averymarques4@gmail.com>
2020-08-08 00:39:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,7 @@
 def prime?(number)
   return false if number < 0
     (2..(number - 1)).each do |n|
-      return false if number % n == 0
-      
+      return false if number % n == 0    
     end
     true
   end
